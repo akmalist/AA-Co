4edc55ebe444cff746e3594f9d7b25e6e4f9f732
Aki <akmalqa101@gmail.com>
2020-01-11 13:59:16 -0800

    created component for menu and directory

---
+++ src/components/directory/directory-menu.component.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export class Directory extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            sections = []
+        }
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default  Directory;
