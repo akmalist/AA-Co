d5fac2012c7e8985e827a7e5243c378ba1242f52
Aki <akmalqa101@gmail.com>
2020-01-14 14:02:36 -0800

    fix menu item and directory components

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,18 @@
-import React from "react";
-import "./menu-item.styles.scss"
+import React from 'react';
+import './menu-item.styles.scss';
 
-export const MenuItem = ({title})=> {
-	        <div className="menu-item">
-				<div className="content">
-					<h1 className="title"> {title}</h1>
-					<span className="subtitle">Shop Now</span>
-				</div>
-			</div>
-}
+const MenuItem = ({ title, img, size }) => (
+	<div
+	style={{
+		backgroundImage:  `url(${img})`
+	}}
+	className={`${size} menu-item`}
+	>
+		<div className="content">
+			<h1 className="title"> {title}</h1>
+			<span className="subtitle">Shop Now</span>
+		</div>
+	</div>
+)
+
+export default MenuItem;
