9c60d7b10e26fa1af4295f481ade5086810e4023
Aki <akmalqa101@gmail.com>
2020-01-17 07:49:44 -0800

    fix yarn issues

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+  
 import React from 'react';
-// import './App.css';
+import { Switch, Route } from 'react-router-dom';
+
+import './App.css';
+
 import HomePage from '../src/pages/homepage.component';
 
-function App() {
+ 
+const HatsPage = () => (
+	<div>
+	  <h1>HATS PAGE </h1>
+	</div>
+  );
+  
+  function App() {
 	return (
-		<div>
-			<HomePage />
-		</div>
+	  <div>
+		<Switch>
+		  <Route exact path='/' component={HomePage} />
+		  <Route path='/hats' component={HatsPage} />
+		</Switch>
+	  </div>
 	);
-}
-
-export default App;
+  }
+  
+  export default App;
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, img, size }) => (
-	<div className={`${size} menu-item`} >
+import {withRouter} from "react-router-dom"
+
+const MenuItem = ({ title, imageUrl, size, history, match,linkUrl}) => (
+	<div className={`${size} menu-item`} 
+	onClick = { ()=> history.push(`${match.url}${linkUrl}`)}
+	>
 		<div className="background-image"	style={{
-		backgroundImage:  `url(${img})`
+		backgroundImage:  `url(${imageUrl})`
 	}}/>
 		<div className="content">
 			<h1 className="title"> {title.toUpperCase()}</h1>
-			<span className="subtitle">Shop Now</span>
+			<span className="subtitle">SHOP NOW</span>
 		</div>
 	</div>
 )
 
-export default MenuItem;
+export default  withRouter(MenuItem);
+
