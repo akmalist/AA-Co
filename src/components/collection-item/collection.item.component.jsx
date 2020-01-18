54de0153d7e5a707dc02d33a8d73a8543feae58c
Aki <akmalqa101@gmail.com>
2020-01-18 11:14:10 -0800

    created a header component for all pages

--- src/components/collection-item/collection.item.component.jsx
+++ src/components/collection-item/collection.item.component.jsx
@@ -6,7 +6,7 @@ const CollectionItem = ({ id, name, imageUrl, price }) => (
 	<div key={id} className="collection-item">
         <div className="image"
          style={{ backgroundImage: `url(${imageUrl})` }} />
-		<div name ="collection-footer">
+		<div className ="collection-footer">
 			<span className="name">{name}</span>
 			<span className="price">{price}</span>
 		</div>
